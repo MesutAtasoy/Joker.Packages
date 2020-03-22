d3aee72950d6f114bddfa391c7ed109868cf193f
Mesut <mesutatasoy15@gmail.com>
2020-03-22 15:35:52 +0300

    PostgreSQL is implementesd in Joker.EntityFrameworkCore library

---
+++ Joker.EntityFramaworkCore/OptionsBuilders/JokerDbContextOptionBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Joker.EntityFramaworkCore.OptionsBuilders
+{
+    public class JokerDbContextOptionBuilder
+    {
+        public string ConnectionString { get; set; }
+        public bool EnableMigration { get; set; }
+        public int MaxRetryCount { get; set; }
+        public TimeSpan? MaxRetryDelay { get; set; }
+    }
+}
--- Joker.EntityFramaworkCore/ServiceCollectionExtensions.cs
+++ Joker.EntityFramaworkCore/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Joker.EntityFramaworkCore.OptionsBuilders;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,11 +9,9 @@ namespace Joker.EntityFramaworkCore
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddJokerDbContext<TContext>(this IServiceCollection services,
-            Action<SqlServerDbContextOptionsBuilder> sqlServerOptionsAction)
+        public static IServiceCollection AddJokerDbContext<TContext>(this IServiceCollection services, Action<SqlServerDbContextOptionsBuilder> sqlServerOptionsAction)
             where TContext : DbContext
         {
-
             IConfiguration configuration;
             using (var serviceProvider = services.BuildServiceProvider())
             {
@@ -22,14 +21,16 @@ namespace Joker.EntityFramaworkCore
             services.AddDbContext<TContext>(options =>
             {
                 options.UseSqlServer(configuration["ConnectionString"], sqlServerOptionsAction);
-
             });
+
             return services;
         }
 
-        public static IServiceCollection AddJokerDbContext<TContext>(this IServiceCollection services)
-            where TContext : DbContext
+        public static IServiceCollection AddJokerDbContext<TContext>(this IServiceCollection services, Action<JokerDbContextOptionBuilder> optionBuilder)
+           where TContext : DbContext
         {
+            JokerDbContextOptionBuilder contextOptionBuilder = new JokerDbContextOptionBuilder();
+            optionBuilder.Invoke(contextOptionBuilder);
 
             IConfiguration configuration;
             using (var serviceProvider = services.BuildServiceProvider())
@@ -37,20 +38,53 @@ namespace Joker.EntityFramaworkCore
                 configuration = serviceProvider.GetService<IConfiguration>();
             }
 
+            if (string.IsNullOrEmpty(contextOptionBuilder.ConnectionString))
+                throw new ArgumentNullException("Connectionstring can not be null", nameof(contextOptionBuilder.ConnectionString));
+
             string assemblyName = typeof(TContext).Namespace;
 
             services.AddDbContext<TContext>(options =>
             {
-                options.UseSqlServer(configuration["ConnectionString"], sqlOptions =>
+                options.UseSqlServer(contextOptionBuilder.ConnectionString, sqlOptions =>
                 {
-                    sqlOptions.MigrationsAssembly(assemblyName);
-                    sqlOptions.EnableRetryOnFailure(maxRetryCount: 10,
-                        maxRetryDelay: TimeSpan.FromSeconds(30),
-                        errorNumbersToAdd: null);
-
-                }
-                );
+                    if (contextOptionBuilder.EnableMigration)
+                    {
+                        sqlOptions.MigrationsAssembly(assemblyName);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: contextOptionBuilder.MaxRetryCount,
+                            maxRetryDelay: contextOptionBuilder.MaxRetryDelay ?? TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null);
+                    }
+                });
             });
+            return services;
+        }
+
+        public static IServiceCollection AddJokerNpDbContext<TContext>(this IServiceCollection services, Action<JokerDbContextOptionBuilder> optionBuilder)
+         where TContext : DbContext
+        {
+            JokerDbContextOptionBuilder contextOptionBuilder = new JokerDbContextOptionBuilder();
+            optionBuilder.Invoke(contextOptionBuilder);
+
+            IConfiguration configuration;
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                configuration = serviceProvider.GetService<IConfiguration>();
+            }
+
+            if (string.IsNullOrEmpty(contextOptionBuilder.ConnectionString))
+                throw new ArgumentNullException("Connectionstring can not be null", nameof(contextOptionBuilder.ConnectionString));
+
+            string assemblyName = typeof(TContext).Namespace;
+
+            services.AddDbContext<TContext>(options =>
+            {
+                options.UseNpgsql(contextOptionBuilder.ConnectionString, sqlOptions =>
+                {
+                    if (contextOptionBuilder.EnableMigration)
+                        sqlOptions.MigrationsAssembly(assemblyName);
+                });
+            });
+
             return services;
         }
     }
