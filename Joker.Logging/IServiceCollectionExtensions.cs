f25c0351dc62e5484d3a30aef122ee646136db42
Mesut <mesutatasoy15@gmail.com>
2020-03-22 12:34:40 +0300

    Loggin and WebHost

---
+++ Joker.Logging/IServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Joker.Logging.Models;
+using Joker.Shared.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace Joker.Logging
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddElkLogging(this IServiceCollection services)
+        {
+            IConfiguration configuration;
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                configuration = serviceProvider.GetService<IConfiguration>();
+            }
+
+            services.AddOption<ElkOptions>("elk");
+            services.Configure<ElkOptions>(configuration);
+            return services;
+        }
+
+        public static Serilog.ILogger CreateSerilogLogger(IConfiguration configuration, string AppName)
+        {
+            var logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithProperty("ApplicationName", AppName)
+            .WriteTo.Elasticsearch(
+                new ElasticsearchSinkOptions(new Uri(configuration["elk:Url"]))
+                {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = configuration["elk:indexFormat"],
+                    CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true)
+                });
+
+            var isEnabled = configuration.GetValue<bool>("logger:enabled");
+            if (isEnabled)
+                logger.WriteTo.MSSqlServer(configuration["logger:connectionString"], configuration["logger:tableName"]);
+
+            return logger.CreateLogger();
+        }
+    }
+}
---
+++ Joker.WebHost/IWebHostExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Polly;
+using System;
+using System.Data.SqlClient;
+
+namespace Joker.WebHost
+{
+    public static class IWebHostExtensions
+    {
+
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+
+                    var retry = Policy.Handle<SqlException>()
+                         .WaitAndRetry(new TimeSpan[]
+                         {
+                             TimeSpan.FromSeconds(3),
+                             TimeSpan.FromSeconds(5),
+                             TimeSpan.FromSeconds(8),
+                         });
+
+                    retry.Execute(() => InvokeSeeder(seeder, context, services));
+
+
+                    logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+                }
+            }
+
+            return webHost;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services)
+            where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+
+}
