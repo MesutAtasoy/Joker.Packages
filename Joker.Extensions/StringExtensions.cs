d94c2b17cea376bce6b6937c1a2090edace6aac6
Mesut <mesutatasoy15@gmail.com>
2020-05-03 20:04:48 +0300

    GenerateSlug is added in String Extensions

--- Joker.Extensions/StringExtensions.cs
+++ Joker.Extensions/StringExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Html;
 
@@ -48,6 +49,35 @@ namespace Joker.Extensions
         public static HtmlString ToHtmlString(this string str)
         {
             return new HtmlString(str);
+        }
+        
+        public static string CreateUniqueCode()
+        {
+            Guid g = Guid.NewGuid();
+            string s = Convert.ToBase64String(g.ToByteArray());
+            s = s.Replace("=", "");
+            s = s.Replace("+", "");
+            return s;
+        }
+
+        public static string GenerateSlug(this string phrase)
+        {
+            string str = phrase.RemoveAccent().ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+
+        public static string RemoveAccent(this string txt)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return System.Text.Encoding.ASCII.GetString(bytes);
         }
     }
 }
