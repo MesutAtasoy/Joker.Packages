746408e16164b6e108276db7096d407beeab2959
Mesut <mesutatasoy15@gmail.com>
2020-09-17 21:10:55 +0300

    Cache Attribute is added.

---
+++ Joker.Cache/Attributes/ICacheAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Joker.Cache.Attributes
+{
+    public interface ICacheAttribute
+    {
+        Task<string> OnBeforeAsync(MethodInfo targetMethod, object[] args, IJokerDistributedCache distributedCache);
+        string OnBefore(MethodInfo targetMethod, object[] args, IJokerDistributedCache distributedCache);
+        Task  OnAfterAsync(MethodInfo targetMethod,  object[] args, object value,  IJokerDistributedCache distributedCache);
+        void  OnAfter(MethodInfo targetMethod,  object[] args, object value,  IJokerDistributedCache distributedCache);
+    }
+}
