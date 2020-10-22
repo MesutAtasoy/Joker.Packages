ed0bebf031f032fd9e9c211b078ef13620f4cc64
Mesut <mesutatasoy15@gmail.com>
2020-10-22 22:06:36 +0300

    Rabbit MQ Client is updated.

---
+++ Joker.EventBusRabbitMQ/ProducerBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace Joker.EventBusRabbitMQ
+{
+    public interface IRabbitMqProducer<in T>
+    {
+        void Publish(T @event);
+        Task PublishAsync(T @event);
+    }
+
+    public abstract class ProducerBase<T> : RabbitMqClientBase, IRabbitMqProducer<T>
+    {
+        private readonly ILogger<ProducerBase<T>> _logger;
+        protected abstract string ExchangeName { get; }
+        protected abstract string RoutingKeyName { get; }
+        protected abstract string AppId { get; }
+
+        protected ProducerBase(
+            ConnectionFactory connectionFactory,
+            ILogger<RabbitMqClientBase> logger,
+            ILogger<ProducerBase<T>> producerBaseLogger) :
+            base(connectionFactory, logger) => _logger = producerBaseLogger;
+
+        public virtual void Publish(T @event)
+        {
+            try
+            {
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event));
+                var properties = Channel.CreateBasicProperties();
+                properties.AppId = AppId;
+                properties.ContentType = "application/json";
+                properties.DeliveryMode = 1;
+                properties.Timestamp = new AmqpTimestamp(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+                Channel.BasicPublish(exchange: $"{ExchangeName}", routingKey: RoutingKeyName, body: body, basicProperties: properties);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "Error while publishing");
+            }
+        }
+
+        public Task PublishAsync(T @event)
+        {
+            try
+            {
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event));
+                var properties = Channel.CreateBasicProperties();
+                properties.AppId = AppId;
+                properties.ContentType = "application/json";
+                properties.DeliveryMode = 1;
+                properties.Timestamp = new AmqpTimestamp(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+                Channel.BasicPublish(exchange: $"{ExchangeName}", routingKey: RoutingKeyName, body: body, basicProperties: properties);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "Error while publishing");
+            }
+            
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Joker.EventBusRabbitMQ/RabbitMqClientBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace Joker.EventBusRabbitMQ
+{
+    public abstract class RabbitMqClientBase : IDisposable
+    {
+        protected abstract string VirtualHost { get; }
+        protected abstract string Exchange { get; }
+        protected abstract string Queue { get; }
+        protected abstract string QueueAndExchangeRoutingKey { get; }
+
+        protected IModel Channel { get; private set; }
+        private IConnection _connection;
+        private readonly ConnectionFactory _connectionFactory;
+        private readonly ILogger<RabbitMqClientBase> _logger;
+
+        protected RabbitMqClientBase(ConnectionFactory connectionFactory,
+            ILogger<RabbitMqClientBase> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+            ConnectToRabbitMq();
+        }
+
+        private void ConnectToRabbitMq()
+        {
+            if (_connection == null || _connection.IsOpen == false)
+            {
+                _connection = _connectionFactory.CreateConnection();
+            }
+
+            if (Channel == null || Channel.IsOpen == false)
+            {
+                Channel = _connection.CreateModel();
+                Channel.ExchangeDeclare(exchange: Exchange, type: "direct", durable: true, autoDelete: false);
+                Channel.QueueDeclare(queue: Queue, durable: false, exclusive: false, autoDelete: false);
+                Channel.QueueBind(queue: Queue, exchange: Exchange, routingKey: QueueAndExchangeRoutingKey);
+            }
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                Channel?.Close();
+                Channel?.Dispose();
+                Channel = null;
+
+                _connection?.Close();
+                _connection?.Dispose();
+                _connection = null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "Cannot dispose RabbitMQ channel or connection");
+            }
+        }
+    }
+}
