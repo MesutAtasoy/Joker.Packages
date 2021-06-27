7d35a227d6113d2c13b1a2c2b9417d7fdf5fbbc5
mesut <mesutatasoy15@gmail.com>
2021-06-27 18:29:27 +0300

    LoggerConfigurationFactory Added

---
+++ Joker.Logging/LoggerConfigurationFactory.cs
@@ -0,0 +1,24 @@
+using System;
+using Joker.Logging.Models;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Joker.Logging
+{
+    public class LoggerConfigurationFactory
+    {
+        public static ILogger CreateSerilogLogger(IConfiguration configuration, Action<SeqLoggerOptions> optionBuilder)
+        {
+            var loggerOptions = new SeqLoggerOptions();
+            optionBuilder.Invoke(loggerOptions);
+            
+            string applicationName = configuration["Serilog:ApplicationName"];
+            
+            return new LoggerConfiguration()
+                .Enrich.WithProperty("ApplicationContext", loggerOptions.AppName)
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+    }
+}
