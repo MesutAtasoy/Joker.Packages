14804943c1dbc9ab1063597e29fc36c9730db450
Mesut <mesutatasoy15@gmail.com>
2021-11-16 22:01:51 +0300

    MigrateDbContext Override

--- Joker.EntityFrameworkCore.Migration/MigrationExtensions.cs
+++ Joker.EntityFrameworkCore.Migration/MigrationExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Polly;
 using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 
 namespace Joker.EntityFrameworkCore.Migration;
@@ -50,6 +51,45 @@ public static class MigrationExtensions
         }
 
         return webHost;
+    }
+    
+    public static async Task MigrateDbContext<TContext>(this IApplicationBuilder applicationBuilder,
+        Action<TContext, IServiceProvider> seeder) where TContext
+        : DbContext
+    {
+        await using var scope = applicationBuilder.ApplicationServices.CreateAsyncScope();
+        var services = scope.ServiceProvider;
+
+        var logger = services.GetRequiredService<ILogger<TContext>>();
+
+        var context = services.GetService<TContext>();
+
+        try
+        {
+            //Test Commit
+            logger.LogInformation("Migrating database associated with context {DbContextName}",
+                typeof(TContext).Name);
+
+
+            var retry = Policy.Handle<SqlException>()
+                .WaitAndRetry(new TimeSpan[]
+                {
+                    TimeSpan.FromSeconds(3),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(8),
+                });
+
+            retry.Execute(() => InvokeSeeder(seeder, context, services));
+
+
+            logger.LogInformation("Migrated database associated with context {DbContextName}",
+                typeof(TContext).Name);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}",
+                typeof(TContext).Name);
+        }
     }
 
     private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context,
